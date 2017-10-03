c8a331604d92d6d4a4bf024e1bf6306c1f5376de
osobis <osobis@gmail.com>
2017-10-03 09:18:03 +0300

    Added install requirements to setup

--- src/setup.py
+++ src/setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='kuorimo',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     author='Leszek Skoczylas',
-    install_requires=[],
+    install_requires=['curse-menu', 'xlsxwriter'],
     author_email='leszek.skoczylas@mac.com',
     description='Kuorimo Order System',
     entry_points={'console_scripts': [
